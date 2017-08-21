13092e23d390949e47d6e9e4f1cfef66761bcfa8
MaryAlex <worker96@mail.ru>
2017-08-21 16:49:50 +0300

    Change hash function

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
 import random
 
-from Utils import is_win, hash_md5
+from Utils import is_win, HMAC_SHA
 
 RANDOM_TO_NUMBER = 10000
 
 computers_number = random.randrange(RANDOM_TO_NUMBER)
-print(hash_md5(computers_number))
+print(HMAC_SHA(computers_number))
 player_choose = int(input("You choose?(Stone = 0, Spock = 1, Paper = 2, Lizard = 3, Scissors = 4)\n"))
 print(is_win(player_choose, computers_number))
